89174ab372733b6d27bc79dccc3605ddb0e9696d
jadson179 <jadson44.santos@gmail.com>
2020-09-17 20:57:09 -0300

    Adicionando modelagem da base de dados

---
+++ backend/knexfile.ts
@@ -0,0 +1,38 @@
+require('ts-node/register');
+
+module.exports = {
+  development: {
+    client: "sqlite3",
+    connection: {
+      filename: `${__dirname}/src/database/database.db`,
+    },
+    useNullAsDefault: true,
+    migrations: {
+      tableName: "knex_migrations",
+      directory: `${__dirname}/src/database/migrations`,
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`,
+    },
+    timezone: "UTC",
+  },
+  production: {
+    client: "mysql",
+    connection: {
+      port: process.env.DATABASE_PORT,
+      host: process.env.DATABASE_HOST,
+      database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_ACCESS_KEY,
+    },
+    useNullAsDefault: true,
+    migrations: {
+      tableName: "knex_migrations",
+      directory: `${__dirname}/src/database/migrations`,
+    },
+    seeds: {
+      directory: `${__dirname}/src/database/seeds`,
+    },
+    timezone: "UTC",
+  },
+};
