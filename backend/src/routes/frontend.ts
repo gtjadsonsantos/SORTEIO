4cfebedb10fb04cd24bcf68ec4602d754fefbb06
jadson179 <jadson44.santos@gmail.com>
2020-10-12 23:09:42 -0300

    teste

--- backend/src/routes/frontend.ts
+++ backend/src/routes/frontend.ts
@@ -2,22 +2,21 @@ import { Router } from "express";
 import path from "path";
 const routesFrontend = Router();
 
-routesFrontend.get("/Signin", (req, res) => {
-    
-  res.sendFile(path.resolve("public","index.html"));
-});
 routesFrontend.get("/", (req, res) => {
-    
-  res.sendFile(path.resolve("public","index.html"));
+  res.sendFile(path.resolve("public", "index.html"));
 });
+routesFrontend.get("/Signin", (req, res) => {
+  res.sendFile(path.resolve("public", "index.html"));
+});
+
 routesFrontend.get("/ForgoutPassword", (req, res) => {
-  res.sendFile(path.resolve("public","index.html"));
+  res.sendFile(path.resolve("public", "index.html"));
 });
 routesFrontend.get("/UserRegistration", (req, res) => {
-  res.sendFile(path.resolve("public","index.html"));
+  res.sendFile(path.resolve("public", "index.html"));
 });
 routesFrontend.get("/Dashboard", (req, res) => {
-  res.sendFile(path.resolve("public","index.html"));
+  res.sendFile(path.resolve("public", "index.html"));
 });
 
 export default routesFrontend;
